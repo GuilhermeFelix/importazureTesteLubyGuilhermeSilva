61f8a206f3458cf9a3f223ff2d5ed9bea30d0e82
Guilherme Felix <1817960@aluno.univesp.br>
2021-01-29 20:34:06 -0300

    Demais entidades da camada de serviço

---
+++ src/Apihorasdesenvolvedor.Servico/Servicos/DesenvolvedorXLancamentohorasService.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Apihorasdesenvolvedor.Dominio.Entidades;
+using Apihorasdesenvolvedor.Dominio.Interfaces;
+using Apihorasdesenvolvedor.Dominio.Interfaces.Servicos.DesenvolvedorXLancamentohoras;
+
+namespace Apihorasdesenvolvedor.Servico.Servicos
+{
+    public class DesenvolvedorXLancamentohorasService : IDesenvolvedorXLancamentohorasService
+    {
+        private IRepositorio<DesenvolvedorXLancamentohorasEntity> _repository;
+        public DesenvolvedorXLancamentohorasService(IRepositorio<DesenvolvedorXLancamentohorasEntity> repositorio)
+        {
+            _repository = repositorio;
+        }
+        public async Task<bool> Delete(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<DesenvolvedorXLancamentohorasEntity> Get(int id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<DesenvolvedorXLancamentohorasEntity>> GetAll()
+        {
+            return await _repository.SelectAsync();
+        }
+
+        public async Task<DesenvolvedorXLancamentohorasEntity> Post(DesenvolvedorXLancamentohorasEntity desenvolvedorXLancamentohoras)
+        {
+            return await _repository.InsertAsync(desenvolvedorXLancamentohoras);
+        }
+
+        public async Task<DesenvolvedorXLancamentohorasEntity> Put(DesenvolvedorXLancamentohorasEntity desenvolvedorXLancamentohoras)
+        {
+            return await _repository.UpdateAsync(desenvolvedorXLancamentohoras);
+        }
+    }
+
+}
